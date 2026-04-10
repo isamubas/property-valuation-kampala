414998f02aaf46a786e82a7085201e58ca7099bb
isamubas <mubiruisaac029@gmail.com>
2026-04-10 16:56:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -76,4 +76,6 @@ with gr.Blocks(title="Kampala Property Valuation") as app:
     )
 
 if __name__ == "__main__":
-    app.launch()
+    app.launch(server_name="0.0.0.0",
+               server_port=7860,
+               share=False)
