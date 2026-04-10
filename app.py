73a73350f52fd59ef1270e2081d6d58e7d8c0555
isamubas <mubiruisaac029@gmail.com>
2026-04-10 18:21:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -78,8 +78,6 @@ if __name__ == "__main__":
     app.launch(
         server_name="0.0.0.0",
         server_port=7860,
-        share=False,
         show_api=False,
         show_error=True,
-        prevent_thread_lock=True
     )
